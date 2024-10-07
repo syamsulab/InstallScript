a2a631b3dfb28990c0b655aacf7fd71aea9d765e
syamsulab <46519797+syamsulab@users.noreply.github.com>
2024-10-07 10:08:01 +0700

    Update odoo_install_18.sh

--- odoo_install_18.sh
+++ odoo_install_18.sh
@@ -7,7 +7,7 @@
 # in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
 # Make a new file:
-# sudo wget https://raw.githubusercontent.com/Yenthe666/InstallScript/16.0/odoo_install.sh
+# sudo wget https://raw.githubusercontent.com/syamsulab/InstallScript/refs/heads/main/odoo_install_18.sh
 # sudo nano odoo_install_18.sh
 # Place this content in it and then make the file executable:
 # sudo chmod +x odoo_install_18.sh
