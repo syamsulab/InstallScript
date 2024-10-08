31330e5afe610490b18ee320756f840890dfe96d
syamsulab <46519797+syamsulab@users.noreply.github.com>
2024-10-08 20:25:56 +0700

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -38,13 +38,13 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="fufufafa.org"
+WEBSITE_NAME="solusimulia.com"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
 ENABLE_SSL="True"
 # Provide Email to register ssl certificate
-ADMIN_EMAIL="fufu@gmail.com"
+ADMIN_EMAIL="caksul@gmail.com"
 ##
 ###  WKHTMLTOPDF download links
 ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
